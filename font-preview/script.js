2984ea0443160869f8d0a328f7e803d9fa2e9d45
reboot13-git <rautkrutik013@gmail.com>
2023-04-29 00:33:54 +0530

    hehe

--- font-preview/script.js
+++ font-preview/script.js
@@ -1,78 +1,68 @@
-// Get the form input and button elements
-const formInput = document.querySelector('.form__input');
-const formButton = document.querySelector('.form__button');
+const fontList = document.getElementById("font-list");
+const textInput = document.getElementById("text-input");
 
-// Get the preview container element
-const previewContainer = document.querySelector('.preview-container');
-
-// Define an array to hold the names of all available fonts
-let fonts = [];
-
-// Function to get all available fonts on the user's device
-async function getFonts() {
-  try {
-    // Ask for permission to access the list of available fonts
-    await document.fonts.ready;
-
-    // Get the list of all available fonts
-    const allFonts = document.fonts;
-
-    // Loop through each font in the list
-    allFonts.forEach(font => {
-      // If the font is not already in the fonts array, add it
-      if (!fonts.includes(font.family)) {
-        fonts.push(font.family);
-      }
+// Ask for permission to access installed fonts
+if (document.fonts && document.fonts.ready) {
+  document.fonts.ready.then(() => {
+    console.log("Fonts loaded");
+  }).catch((err) => {
+    console.error("Error loading fonts", err);
+  });
+} else {
+  // Fallback for older browsers
+  const permissionStatus = document.webkitPermissionRequest ? document.webkitPermissionRequest('font') : undefined;
+  if (permissionStatus === undefined) {
+    console.log("Permission request not supported");
+  } else {
+    permissionStatus.then(() => {
+      console.log("Permission granted");
+    }).catch(() => {
+      console.log("Permission denied");
     });
-  } catch (error) {
-    console.log('Error:', error);
   }
 }
 
-// Function to create a font preview card and append it to the preview container
-function createPreviewCard(fontName, text) {
-  // Create a new div element to hold the font preview card
-  const fontPreview = document.createElement('div');
-  fontPreview.classList.add('font-preview');
+// Generate preview for all installed fonts
+function generatePreview() {
+  const fonts = document.fonts ? document.fonts : undefined;
 
-  // Create a new heading element to display the font name
-  const fontNameElement = document.createElement('h2');
-  fontNameElement.classList.add('font-preview__name');
-  fontNameElement.textContent = fontName;
+  if (!fonts || !fonts.ready) {
+    console.log("Fonts not available");
+    return;
+  }
 
-  // Create a new paragraph element to display the preview text in the selected font
-  const previewTextElement = document.createElement('p');
-  previewTextElement.classList.add('font-preview__text');
-  previewTextElement.textContent = text;
-  previewTextElement.style.fontFamily = fontName;
+  const text = textInput.value.trim();
 
-  // Append the font name and preview text to the font preview card
-  fontPreview.appendChild(fontNameElement);
-  fontPreview.appendChild(previewTextElement);
+  if (text.length === 0) {
+    console.log("Empty text input");
+    return;
+  }
 
-  // Append the font preview card to the preview container
-  previewContainer.appendChild(fontPreview);
-}
+  fontList.innerHTML = "";
 
-// Function to handle form submission
-function handleFormSubmit(event) {
-  // Prevent the form from submitting and refreshing the page
-  event.preventDefault();
+  fonts.ready.then(() => {
+    console.log("Fonts loaded");
 
-  // Clear any previous font previews from the preview container
-  previewContainer.innerHTML = '';
+    fonts.forEach((font) => {
+      const fontName = font.family;
+      const fontPreview = document.createElement("li");
+      fontPreview.classList.add("font-preview");
 
-  // Get the text entered by the user in the form input
-  const text = formInput.value;
+      const fontNameElem = document.createElement("p");
+      fontNameElem.textContent = fontName;
+      fontNameElem.style.fontFamily = fontName;
 
-  // Loop through each font in the fonts array and create a font preview card for it
-  fonts.forEach(font => {
-    createPreviewCard(font, text);
+      const textElem = document.createElement("p");
+      textElem.textContent = text;
+      textElem.style.fontFamily = fontName;
+
+      fontPreview.appendChild(fontNameElem);
+      fontPreview.appendChild(textElem);
+      fontList.appendChild(fontPreview);
+    });
+  }).catch((err) => {
+    console.error("Error loading fonts", err);
   });
 }
 
-// Call the getFonts function to get all available fonts on the user's device
-getFonts();
-
-// Add an event listener to the form button to handle form submission
-formButton.addEventListener('click', handleFormSubmit);
+textInput.addEventListener("input", generatePreview);
